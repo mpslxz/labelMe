8f7e993ec6f29b29abe66dae8eceb6dd08b6fcbe
mpslxz <seyedmehran.pesteie@gmail.com>
2017-11-17 15:18:52 -0800

    ENH: Input arg for root folder

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
 from gui.gui_builder import UI
 from utils.data_utils import b8_to_ndarray, write_overlay
 import Tkinter as tk
+import glob
+import sys
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    file_path = ['tamg_sweep.b8', 'tamb_sweep.b8']
+    try:
+        assert len(sys.argv) > 1
+    except AssertionError as e:
+        raise(AssertionError("Give the root of the files as the input arg."))
 
+    root = tk.Tk()
+    file_path = glob.glob(sys.argv[1] + "/**/*.b8")
+    root.title(
+        "labelMe: A simple python tool for labling b8 images for classification")
     UI = UI(root, file_path, b8_to_ndarray, write_overlay)
     root.mainloop()
