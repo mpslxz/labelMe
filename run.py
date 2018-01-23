b616929fc8a1f1db93751a1adec593861148d119
mpslxz <seyedmehran.pesteie@gmail.com>
2018-01-23 11:25:01 -0800

    ENH: Refactor based on LogFactory mods

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-from gui.gui_builder import UI as CUI
-from gui.line_drawer import UI as LUI
-from utils.data_utils import b8_to_ndarray, write_overlay, line_drawer
 import Tkinter as tk
 import glob
 import sys
+from gui.gui_builder import UI as CUI
+from gui.line_drawer import UI as LUI
+from utils.data_utils import b8_to_ndarray, write_overlay, line_drawer
+from utils.logger import LogFactory
 
 if __name__ == "__main__":
     try:
@@ -12,9 +13,14 @@ if __name__ == "__main__":
         raise(AssertionError("Give the root of the files as the input arg."))
 
     root = tk.Tk()
-    file_path = glob.glob(sys.argv[1] + "/**/*.b8")
+    log_engine = LogFactory(sys.argv[1])
+    file_path = log_engine.get_pending_files_list()
     root.title(
         "labelMe: A simple python tool for labling b8 images")
 
-    UI = LUI(root, file_path, b8_to_ndarray, line_drawer)
+    UI = LUI(root=root,
+             file_paths=file_path,
+             reader_callback=b8_to_ndarray,
+             overlay_callback=line_drawer,
+             logger=log_engine)
     root.mainloop()
